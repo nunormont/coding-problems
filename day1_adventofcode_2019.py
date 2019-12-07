24015f9fca91c30ae68e386862002ebf09505c9b
Nuno Rocha <47053767+nunormont@users.noreply.github.com>
2019-12-07 11:58:11 +0000

    Update day1_adventofcode_2019.py

--- day1_adventofcode_2019.py
+++ day1_adventofcode_2019.py
@@ -1,5 +1,6 @@
 #problem of day 1 of advent from https://adventofcode.com/
 
+
 import math
 f= open("input.txt","r")
 masses=f.readlines()
@@ -14,7 +15,6 @@ tot_fuel=0
 for i in masses:
     fuel=math.floor(int(i)/3)-2
     tot_fuel+=fuel
-    added_fuel=[]
     while math.floor(fuel/3)-2 > 0:
         fuel=math.floor(fuel/3)-2
         tot_fuel+=fuel
